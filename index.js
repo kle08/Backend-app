d7e8e47fa4f1dbb4f1849394476a36e75f7be747
iamasianman <kevinle1991@gmail.com>
2019-09-17 16:55:51 -0700

    Implemented post request

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const Joi = require('joi')
 const express = require('express');
 const app = express();
 const port = 3000;
-
+// middleware 
 app.use(express.json());
 
 const genres = [
@@ -18,9 +19,27 @@ app.get('/api/genres', (req, res) => {
   res.send(genres);
 });
 
-// app.post();
+app.post('api/genres', (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre)
+});
+
 // app.put();
 // app.delete();
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(genre, schema)
+}
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
